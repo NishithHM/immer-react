fecdef4c71159a75b6933a993c61234f49e3ed1f
Nishith <nhm987@gmail.com>
2020-06-19 10:01:27 +0530

    added start.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Case3 from './cases/case3';
 import Case4 from './cases/case4';
 import Case5 from './cases/case5';
 import Case6 from './cases/case6';
-
+import Start from './cases/start';
 function App(props) {
   return (
     <div className="App">
-      <Route render={()=> <Case1 {...props}/>} path='/' exact/>
+      <Route render={()=> <Start {...props}/>} path='/' exact/>
+      <Route render={()=> <Case1 {...props}/>} path='/1' exact/>
       <Route render={()=> <Case2 {...props}/>} path='/2' exact/>
       <Route render={()=> <Case3 {...props}/>} path='/3' exact/>
       <Route render={()=> <Case4 {...props}/>} path='/4' exact/>
